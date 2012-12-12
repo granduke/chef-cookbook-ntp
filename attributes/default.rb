5642ba8a667145404f7f88531612cf5f961bca85
doctorjnupe <jerry@hybridgroup.com>
2012-12-12 14:40:14 -0600

    Updated server references.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,9 +1,9 @@
-e platform
-when "ubuntu","debian"
-  default[:ntp][:service] = "ntp"
-when "redhat","centos","fedora","scientific"
-  default[:ntp][:service] = "ntpd"
+case platform
+  when "ubuntu","debian"
+    default[:ntp][:service] = "ntp"
+  when "redhat","centos","fedora","scientific"
+    default[:ntp][:service] = "ntpd"
 end
 
 default[:ntp][:is_server] = false
-default[:ntp][:servers] = ["ntp1.tfoundry.com iburst", "time.nist.gov"]
+default[:ntp][:servers] = ["time.nsit.gov iburst", "time-nw.nist.gov"]
