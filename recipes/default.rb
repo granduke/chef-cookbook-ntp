9e011d2eabe329d198e63ef543ba2d6e0d00eeb7
doctorjnupe <jerry@hybridgroup.com>
2012-12-18 17:11:10 -0600

    Corrected ntpdate reference.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,33 +1,12 @@
-#
-# Cookbook Name:: ntp
-# Recipe:: default
-#
-# Copyright 2012, YOUR_COMPANY_NAME
-#
-# All rights reserved - Do Not Redistribute
-#
-
-case node[:platform]
-  when "ubuntu","debian"
-    package "ntpdate" do
-      action [:install]
-    end
-    
-    template "/etc/default/ntpdate" do
-      source "ntpdate.default.erb"
-      owner "root"
-      group "root"
-      mode 0644
-      notifies :restart, "service[ntp]"
-      variables(
-        :disable => node['ntp']['ntpdate']['disable']
-       )
-    end
+case node[:platform] 
+when "ubuntu","debian"
+  package "ntpdate" do
+    action :install
   end
-
+end
 
 package "ntp" do
-  action [:install]
+  action :install
 end
 
 template "/etc/ntp.conf" do
